2ce2eec1370c0e790dfcc3c4cd4c25dff70ae763
addypradip <addypradip@users.noreply.github.com>
2020-12-23 00:34:37 -0600

    Third Commit

---
+++ Date.js
@@ -0,0 +1,9 @@
+const now = new Date();
+const date1 = new Date('June 9 2019 08:00');
+const date2 = new Date(2019, 4, 11, 9, 0);
+
+console.log(now);
+console.log(date1);
+console.log(now.getDate());
+console.log(now.toDateString());
+console.log(now.toTimeString());
